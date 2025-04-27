74cb01618d33268c8986f66a16789dc9fe1abb35
Eraldo Caetano <eraldocaetano1999@gmail.com>
2025-04-27 16:17:53 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!qqq'
+@app.route("/")
+def inicio():
+    return render_template("index.html")
 
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/resultado", methods=["POST"])
+def resultado():
+    return 'O resultado é: '+ str(int(request.form["n1"])+int(request.form["n2"]))
+
+@app.route("/autor")
+def autor():
+    return render_template("autor.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
